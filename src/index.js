7e5acaf3e5de0b6248796ce1975e59cf6ac275dd
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2018-05-01 17:48:17 -0500

    React Youtube Clone
    Youtube Clone made during react redux course.

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCNLm4Qf8Xj-p05A1tDxBJabhRTZKDe1po';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+
+     };
+
+     this.videoSearch('surfboards');
+   }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos)=>{
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+     });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300);
+    
+    return (
+    <div>
+      <SearchBar onSearchTermChange={videoSearch} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+        videos={this.state.videos} />
+    </div>);
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
